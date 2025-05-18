f185bdece8f5d9cae2040857eb7ce8da18231664
BunnyeNyash <otachibosibori@gmail.com>
2025-05-18 14:20:39 +0300

    A Python script for lazy loading paginated data from user_data table

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -11,7 +11,7 @@ import seed
 def paginate_users(page_size, offset):
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
-    cursor.execute(f"SELECT user_id, name, email, age FROM user_data LIMIT {page_size} OFFSET {offset}")
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
     rows = cursor.fetchall()
     connection.close()
     return rows
