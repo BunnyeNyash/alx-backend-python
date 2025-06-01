711c4250c467de287bbd8604d0b3b1554b664cd1
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-01 22:15:10 +0300

    Task 2: Create serializers to define the many to many relationships

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -7,16 +7,26 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number']
 
 class MessageSerializer(serializers.ModelSerializer):
-    sender = UserSerializer(read_only=True)  # Nested serializer for sender
+    sender = UserSerializer(read_only=True)
+    sender_id = serializers.UUIDField(write_only=True)
+
     class Meta:
         model = Message
-        fields = ['message_id', 'conversation', 'sender', 'message_body', 'sent_at']
-        read_only_fields = ['message_id', 'sender', 'sent_at']
+        fields = ['message_id', 'conversation', 'sender', 'sender_id', 'message_body', 'sent_at']
+        read_only_fields = ['message_id', 'sent_at']
 
 class ConversationSerializer(serializers.ModelSerializer):
-    participants = UserSerializer(many=True, read_only=True)  # Nested serializer for participants
-    messages = MessageSerializer(many=True, read_only=True)  # Nested serializer for messages
+    participants = UserSerializer(many=True, read_only=True)
+    participant_ids = serializers.ListField(child=serializers.UUIDField(), write_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Conversation
-        fields = ['conversation_id', 'participants', 'messages', 'created_at', 'updated_at']
+        fields = ['conversation_id', 'participants', 'participant_ids', 'created_at', 'updated_at', 'messages']
         read_only_fields = ['conversation_id', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        participant_ids = validated_data.pop('participant_ids')
+        conversation = Conversation.objects.create()
+        conversation.participants.set(User.objects.filter(user_id__in=participant_ids))
+        return conversation
