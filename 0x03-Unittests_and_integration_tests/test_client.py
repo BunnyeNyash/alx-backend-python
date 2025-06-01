da8a24339d6bdba4080d9e9c6337fb30aac7405c
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-01 08:17:44 +0300

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -30,3 +30,20 @@ class TestGithubOrgClient(unittest.TestCase):
             client = GithubOrgClient("test_org")
             self.assertEqual(client._public_repos_url, test_payload["repos_url"])
             mock_org.assert_called_once()
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Test GithubOrgClient.public_repos returns expected repo list."""
+        test_repos_url = "http://example.com/repos"
+        test_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"}
+        ]
+        mock_get_json.return_value = test_payload
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = test_repos_url
+            client = GithubOrgClient("test_org")
+            self.assertEqual(client.public_repos(), ["repo1", "repo2"])
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(test_repos_url)
