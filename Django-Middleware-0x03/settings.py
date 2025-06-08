c3163dd621c4d47e95526a58d89012a66d061822
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-08 23:25:03 +0300

    Update settings.py

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -81,6 +81,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware',
     'chats.middleware.RestrictAccessByTimeMiddleware',
+    "chats.middleware.OffensiveLanguageMiddleware",
     'chats.middleware.OffensiveLanguageMiddleware',
     'chats.middleware.RolePermissionMiddleware',
 ]
