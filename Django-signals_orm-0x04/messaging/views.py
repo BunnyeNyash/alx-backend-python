dbdc6fcb0c341cf0fde84cfed385fd88111fae1f
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-15 13:56:40 +0300

    Task 5

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.models import User
 from messaging.models import Message
+from django.views.decorators.cache import cache_page
 
 @login_required
 def delete_user(request):
@@ -24,5 +25,11 @@ def threaded_conversation(request, message_id):
 def inbox(request):
     unread_messages = Message.unread.unread_for_user(request.user)
     return render(request, 'messaging/inbox.html', {'unread_messages': unread_messages})
-    
+
+@cache_page(60)  # Cache for 60 seconds
+def conversation_list(request):
+    messages = Message.objects.filter(receiver=request.user).select_related('sender')
+    return render(request, 'messaging/conversation.html', {'messages': messages})
+m
+
 # Create your views here.
