87edfbcac6214c8cbee1ab8c27a22f880e0a81da
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-15 12:16:50 +0300

    Update settings.py

--- Django-signals_orm-0x04/messaging_app/settings.py
+++ Django-signals_orm-0x04/messaging_app/settings.py
@@ -10,17 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))        # Load .env file
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%n0mr#%m26wwn)=0a))!_q^u-s5-wgx&gey6ge%*(mz%k=)*o-'
+# SECURITY WARNING: keep the secret key used in production secret! {python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"}
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
