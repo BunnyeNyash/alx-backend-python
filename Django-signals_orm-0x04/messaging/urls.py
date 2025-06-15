41ab62d029aae8571f42143d6fa4e97705954201
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-15 14:56:42 +0300

    This file defines app-level URL routing for views.

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('inbox/', views.inbox, name='inbox'),  # Task 4
     path('thread/<int:message_id>/', views.threaded_conversation, name='threaded_conversation'),  # Task 3
     path('delete-account/', views.delete_user, name='delete_user'),  # Task 2
+    path('message/<int:message_id>/history/', views.message_edit_history, name='message_edit_history'),    # edit history view
 ]
