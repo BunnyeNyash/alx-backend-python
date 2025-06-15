95f0c5cf62653bd8573c0c3f4b4172a7d13161e9
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-15 13:38:43 +0300

    creating views

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+@login_required
+def delete_user(request):
+    user = request.user
+    user.delete()
+    return HttpResponseRedirect(reverse('home'))  # Redirect after deletion
 
 # Create your views here.
