17f03c5be0b7896e1d5618810c6d6f9e5a39e23d
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-15 14:19:10 +0300

    This file defines app-level URL routing for views.

---
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('conversation/', views.conversation_list, name='conversation_list'),  # Task 5
+    path('inbox/', views.inbox, name='inbox'),  # Task 4
+    path('thread/<int:message_id>/', views.threaded_conversation, name='threaded_conversation'),  # Task 3
+    path('delete-account/', views.delete_user, name='delete_user'),  # Task 2
+]
