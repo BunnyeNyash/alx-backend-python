79cdcb01b69bc14304bee180e5aa1101a531c082
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-15 12:26:56 +0300

    Registering models

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from messaging.models import Message, Notification
 
-# Register your models here.
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['sender', 'receiver', 'content', 'timestamp']
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'message', 'created_at', 'is_read']
