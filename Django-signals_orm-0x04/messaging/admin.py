655c6b34c56d951ed79e57de2d053e9df469abfe
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-15 13:07:22 +0300

    Adding MessageHistory

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from messaging.models import Message, Notification
+from messaging.models import Message, Notification, MessageHistory
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
@@ -8,3 +8,7 @@ class MessageAdmin(admin.ModelAdmin):
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
     list_display = ['user', 'message', 'created_at', 'is_read']
+
+@admin.register(MessageHistory)
+class MessageHistoryAdmin(admin.ModelAdmin):
+    list_display = ['message', 'old_content', 'edited_at']
